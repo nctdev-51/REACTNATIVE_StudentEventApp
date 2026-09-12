336dbe3d10707282bc5acb4b85acb9363bcfd8aa
nctdev <tamznguyen15@gmail.com>
2026-09-12 23:33:24 +0700

    Tam

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { TamaguiProvider } from "tamagui";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Project Base Nhóm 03</Text>
+        <Text>...</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
 });
---
+++ README.md
@@ -0,0 +1,11 @@
+Tui chưa setup gì hết nha mọi người xài gì thì cài rùi note vô đây hay nhắn vô nhóm cũng được nhưng mà chỉ xài trong khuôn khổ của thầy nha
+
+B1:
+git clone https://github.com/TaikhoanCuaBan/StudentEventApp.git
+cd StudentEventApp
+npm install
+
+B2: Tạo nhánh mới cho riêng mình nha
+git checkout -b feature/(tên của mng)
+
+B3: code xong commit lên như bình thường
